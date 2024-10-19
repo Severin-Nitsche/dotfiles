a9a42a24a2f16ef05947a34a76499aba8be7b8bc
Severin Nitsche <severinnitsche@MacBook-Pro-von-Severin.fritz.box>
2024-10-19 20:25:45 +0200

    feat: Add home manager config

--- flake.nix
+++ flake.nix
@@ -5,12 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, darwin }: {
     darwinConfigurations."sevs-macbook-pro" = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
       modules = [ ./hosts/sevs-macbook-pro/configuration.nix ];
+    };
+
+    homeConfigurations."severinnitsche" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+      modules = [ ./home/severinnitsche/home.nix ];
     };
   };
 }
