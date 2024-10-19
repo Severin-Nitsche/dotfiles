5d7ec96bc9132191aaa8ab3f09c9ddee58976291
Severin Nitsche <severinnitsche@MacBook-Pro-von-Severin.fritz.box>
2024-10-19 22:17:06 +0200

    feat: allow unfree packages

--- home/severinnitsche/home.nix
+++ home/severinnitsche/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nixpkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -14,6 +14,8 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  nixpkgs.config.allowUnfree = true;
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
