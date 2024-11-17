396e6fce2024b3e6ba2d197b6b33579b2743405e
Severin Nitsche <severinnitsche@gmail.com>
2024-11-17 11:22:12 +0100

    feat: Set kblayout to de in sddm

--- hosts/sevs-kekbook-pro/configuration.nix
+++ hosts/sevs-kekbook-pro/configuration.nix
@@ -60,7 +60,7 @@
   
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "de";
+  services.xserver.xkb.layout = "de";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
