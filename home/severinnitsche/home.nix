35e3c8cc4295891e53ad133a15c7e8124b6555c7
Severin Nitsche <severinnitsche@gmail.com>
2025-04-29 09:41:14 +0200

    chore: Add git.rwth-aachen.de private key

--- home/severinnitsche/home.nix
+++ home/severinnitsche/home.nix
@@ -79,6 +79,9 @@
       Host git-ce.rwth-aachen.de
         AddKeysToAgent yes
         IdentityFile ~/.ssh/git-ce
+      Host git.rwth-aachen.de
+        AddKeysToAgent yes
+        IdentityFile ~/.ssh/gitlab
     '';
   };
 
