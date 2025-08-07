b0ddd52dc9599c74e3094f6810bfb497fd06b847
Severin Nitsche <severinnitsche@gmail.com>
2025-08-07 23:57:59 +0200

    feat: Configure sudoers file
    * vpn wo/ double password
    * darwin-rebuild wo/ password
    
    * May need to refactor vpn-specific into more general

--- hosts/sevs-macbook-pro/configuration.nix
+++ hosts/sevs-macbook-pro/configuration.nix
@@ -14,6 +14,9 @@
       "nix-command" 
       "flakes" 
     ];
+    nixpkgs.overlays = with (import ../../overlays); [
+      additions
+    ];
   
     # Some stuff in nix darwin is deprecated / up to change
     # For now, I need this for compatibility
@@ -26,6 +29,11 @@
   
     # No more password for sudo
     security.pam.services.sudo_local.touchIdAuth = true;
+    security.sudo.extraConfig = ''
+      severinnitsche ALL=(ALL) NOPASSWD:/run/current-system/sw/bin/darwin-rebuild
+      severinnitsche ALL=(ALL) NOPASSWD:/usr/bin/killall openconnect
+      severinnitsche ALL=(ALL) NOPASSWD:${pkgs.openconnect}/bin/openconnect
+    '';
 
     system.stateVersion = 5;
   };
