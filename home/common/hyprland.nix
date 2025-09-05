9383e1b8f9ecfd0c0ff286ada7c4db5bb59dcd00
Severin Nitsche <severinnitsche@gmail.com>
2025-09-05 22:32:17 +0200

    feat: Configure touchpad

--- home/common/hyprland.nix
+++ home/common/hyprland.nix
@@ -21,8 +21,19 @@
         "$mod, M, exec, uwsm stop" # Do not use exit with uwsm
         "$mod, space, exec, uwsm app -- rofi -show drun -run-command 'uwsm app -- {cmd}'"
       ];
+      
       input = {
          kb_file = "./custom.xkb";
+         touchpad = {
+           natural_scroll = true;
+           clickfinger_behavior = true;
+         };
+      };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_min_fingers = true;
+        workspace_swipe_min_speed_to_force = 15;
+        workspace_swipe_cancel_ratio = 0.05;
       };
     };
     home.file.".config/hypr/custom.xkb".source = ./custom.xkb;
