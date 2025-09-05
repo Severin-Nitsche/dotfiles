4d3f281297e4cab14a73f66898f8d2701a0428b5
Severin Nitsche <severinnitsche@gmail.com>
2025-09-05 13:08:56 +0200

    chore: Enable passwordless sudo for nmtui

--- hosts/sevs-kekbook-pro/configuration.nix
+++ hosts/sevs-kekbook-pro/configuration.nix
@@ -18,42 +18,30 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.initrd.postDeviceCommands = lib.mkAfter ''
-  #   mkfs.ext4 -F -L nixos /dev/disk/by-label/nixos
-  # ''; # Does not work because mkfs is not found
 
   nixpkgs.config.allowUnfree = true;
 
-  # Fix wifi (hopefully)
-  # boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "firewire_ohci" "usbhid" "sd_mod" "sr_mod" "sdhci_pci" ];
-  # boot.initrd.kernelModules = [ "wl" ];
-  # boot.kernelModules = [ "kvm-intel" "wl" ];
-  # boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-
   networking.hostName = "sevs-kekbook-pro"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  networking.networkmanager.enable = true;  # Easier than wpa_cli
+  security.sudo.extraRules = [
+    {
+      groups = [ "users" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/nmtui";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "de_DE.UTF-8";
   console.keyMap = "de";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
 
   # Enable sddm as display/login manager
   services.displayManager.sddm.enable = true;
@@ -62,22 +50,15 @@
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "de";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
-    # wireplumber.enable = true; # automatically enabled (used for Hyprland)
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.mutableUsers = true;
@@ -98,15 +79,6 @@
     ];
   };
 
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -115,55 +87,18 @@
   ];
   programs.steam.enable = true; # I hate that this is not working at user site
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  
   # Enable HYPRLAND
   programs.hyprland.enable = true;
   programs.hyprland.withUWSM = true;
   
   services.dbus.implementation = "broker"; # Recommended for uwsm
 
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Do not change
 
 }
 
