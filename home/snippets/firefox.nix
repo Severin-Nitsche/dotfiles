ac618f304ff6b088e7d10891b8e0a18a55603a58
Severin Nitsche <severinnitsche@gmail.com>
2026-05-13 19:35:48 +0200

    feat: Add search engine config
    * Librewolf/Firefox ist bad software
    * On first boot, it fucks with the config and changes the default search
      engine
    * Due to impermanence every boot after reboot is a first boot

--- home/snippets/firefox.nix
+++ home/snippets/firefox.nix
@@ -6,6 +6,44 @@
       id = 0;
       name = "default";
       isDefault = true;
+      search = {
+        order = [ "noai" ];
+        default = "noai";
+        privateDefault = "noai";
+        force = true;
+        engines = {
+          noai = {
+            name = "DuckDuckGo no AI";
+            urls = [{template = "https://noai.duckduckgo.com/?q={searchTerms}";}];
+            iconMapObj."16" = "https://no-ai-icon.com/favicon.ico";
+            definedAliases = [ "@noai" ];
+          };
+          unicode = {
+            name = "Unicode";
+            urls = [{template = "https://www.compart.com/de/unicode/search?q={searchTerms}";}];
+            iconMapObj."16" = "https://www.compart.com/en/unicode/images/icon/favicon.ico";
+            definedAliases = [ "@u" ];
+          };
+          context = {
+            name = "ConTeXt";
+            urls = [{template = "https://wiki.contextgarden.net/index.php?search={searchTerms}";}];
+            iconMapObj."16" = "https://wiki.contextgarden.net/favicon.ico";
+            definedAliases = [ "@context" ];
+          };
+          nix-packages = {
+            name = "Nix Packages";
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}";}
+              ];
+            }];
+            iconMapObj."16" = "https://search.nixos.org/favicon.png";
+            definedAliases = [ "@nixpkgs" "@nix" ];
+          };
+        };
+      };
       settings = {
         "widget.use-xdg-desktop-portal.file-picker" = 1;
         "widget.disable-workspace-management" = true;
