e87407839a7eea6d81e18cfd54157222e81bbd5f
Severin Nitsche <severinnitsche@gmail.com>
2026-05-14 14:11:47 +0200

    feat: Configure Netrw

--- home/snippets/vim.nix
+++ home/snippets/vim.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ lib, ... }: {
 
   imports = [];
 
@@ -10,6 +10,19 @@
     programs.vim.settings.mouse = "a";
     programs.vim.settings.number = true;
     programs.vim.settings.shiftwidth = 2;
+    programs.vim.extraConfig = ''
+      "https://kitsugo.com/guide/netrw-improvements/
+      let g:netrw_banner = 0
+      let g:netrw_altv = 1
+      let g:netrw_browse_split = 4
+      let g:netrw_liststyle = 3
+      let g:netrw_winsize = 20
+
+      augroup InitNetrw
+        autocmd!
+        autocmd VimEnter * if expand("%") == "" | Lexplore | endif
+      augroup END
+    '';
   };
 
 }
