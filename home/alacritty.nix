8e5f10f3073b24584137e51cee699d5ea30f13d1
Severin Nitsche <severinnitsche@gmail.com>
2026-06-14 11:57:09 +0200

    feat: Theme alacritty
    * Unsure whether I want to stick to black/white

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,7 +1,34 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.alacritty.settings = {
     mouse.hide_when_typing = true;
 
     terminal.shell.program = "${pkgs.tmux}/bin/tmux";
+
+    colors = with config.colors; {
+      primary = {
+        background = black;
+        foreground = white;
+      };
+      normal = {
+        black = black;
+        red = norwegian-pink;
+        green = zambia-green;
+        yellow = indian-gold;
+        blue = afghani-blue;
+        magenta = chinese-magenta;
+        cyan = argentinian-blue;
+        white = white;
+      };
+      bright = {
+        black = bright-black;
+        red = bright-norwegian-pink;
+        green = bright-zambia-green;
+        yellow = bright-indian-gold;
+        blue = bright-afghani-blue;
+        magenta = bright-chinese-magenta;
+        cyan = bright-argentinian-blue;
+        white = bright-white;
+      };
+    };
   };
 }
--- modules/colors/colors.nix
+++ modules/colors/colors.nix
@@ -16,6 +16,17 @@
       persian-orange = "#e99861";
       zambia-green = "#6fc488";
       norwegian-pink = "#f08d94";
+      # tints
+      bright-black = "#0b0b0b";
+      bright-white = "#eeeeee";
+      bright-afghani-blue = "#b7cefd";
+      bright-argentinian-blue = "#a4d5f7";
+      bright-italian-violet = "#cbc7fa";
+      bright-indian-gold = "#dfcc99";
+      bright-chinese-magenta = "#e9bee4";
+      bright-persian-orange = "#f3c2a3";
+      bright-zambia-green = "#acdcb8";
+      bright-norwegian-pink = "#f8bcbe";
     };
   };
 
